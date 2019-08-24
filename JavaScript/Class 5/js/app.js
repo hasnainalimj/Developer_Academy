b98c62968f2199f41f1aff14b016cad475088c9b
hasnainalimj <mjhasnain15@gmail.com>
2019-08-24 20:21:11 +0500

    JS Class 5

---
+++ JavaScript/Class 5/js/app.js
@@ -0,0 +1,110 @@
+// alert("hello how are you?");
+// window.alert("hi")
+
+// var name = "Hasnain Ali";
+// alert(name);
+
+// var fullName = "Hello World"; 
+
+// alert(fullName);
+
+// var $use = "hi";
+
+// var _pi = 3.24;
+
+// var A = "a";
+
+// alert(A);
+
+var name; //declaration
+
+name = "King"; //assigning / initializing
+
+var num = "1"-123;
+var num = "1"+123;
+
+// alert(num)
+
+var one = 1;
+
+// one = one + 1;
+
+// alert(one + 2);
+
+// var num1 = 5;
+// var num2 = "5";
+// console.log(num1 % num2);
+
+var roundOf = 5;
+//roundOf++ post increament
+//roundOf-- post decreament
+
+//++roundOf pre increament
+//--roundOf pre decreament
+
+
+// console.log(roundOf++);
+
+// console.log(roundOf)
+
+// console.log(roundOf++)
+// console.log(roundOf)
+
+// console.log(roundOf--);
+
+// console.log(roundOf)
+
+// console.log(roundOf--)
+
+// console.log(roundOf)
+
+var ceilOf = 6;
+
+// console.log(++ceilOf)
+// console.log(--ceilOf)
+
+// var example = 7; 
+
+// var example2 = 6;
+
+// var result1 = ++example - example2++;
+//8 -          6 = 2
+
+// console.log("TCL: result1", result1)
+
+// var result2 = --example - ++example2;
+//7 -  8 = -1
+//2 - 1 = 1 
+// console.log("TCL: result2", result2)
+
+// console.log("this is the answer" , result1 + result2);
+
+// var n1 = 7;
+// var b1 = n1--; //7
+// console.log("TCL: b1", b1)
+
+
+// // console.log(n1;
+// console.log(n1);
+
+
+// var n2 = 8;
+// // n2 = n2++ * 2; //16
+// n2 = n2++; //8
+
+
+// var n3 = 0;
+// n3 = ++n3 + n3--; // 1 + 1 = 2
+
+// // var results = n1 + n2 * n3; // 6 + 9 * 1
+// // console.log("TCL: results", results)
+
+// var totalCost = (1 + 3) * 4;
+
+// var totalCost = 1 + 3 * 4 - 12 / 1;
+
+// var totalCost = (1 + 3) * (4 - 12) / 1;
+
+// console.log("TCL: totalCost", totalCost)
+
+// console.log("$"+"ka chakar")
