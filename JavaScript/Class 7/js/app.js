f070779c9c8ccbcfbf94a725312eb88acddff13d
hasnainalimj <mjhasnain15@gmail.com>
2019-09-01 03:39:56 +0500

    JS Class 7

---
+++ JavaScript/Class 7/js/app.js
@@ -0,0 +1,91 @@
+// var user1 = ""
+// var user2 = ""
+// var user3 = ""
+// var user4 = ""
+// var user5 = ""
+// var user6 = ""
+// var user7 = ""
+// var user8 = ""
+
+
+// var nestedArray = ["@" , "$" , "&"];
+// var otherArray = ['a' , 'b' , 'c' , nestedArray];
+// var username = ["Ali" , "Saad" , "Sami" , "Faizan" , 1 , 2 , 3 , 4 , true , false , [] , otherArray];
+// console.log("TCL: username", username)
+
+// // console.log(username[11][3][1]);
+// username[4] = "Khan";
+// username[10] = "false"
+// console.log("TCL: username", username)
+
+// username[100] = "Hello World";
+
+// console.log("TCL: username", username)
+
+// console.log(username[50]);
+
+//Methods of Array
+//Adding Methods
+// 1) push() adding elements at the end of array // push returns new length of array
+// 2) unshift() adding elements from the beginning of array // unshift returns new length of array
+// 3) pop() removing elements at the end of array // pop returns deleted value
+// 4) shift() removing elements from the beginning of array // shift returns deleted value
+// 5) splice() adding / removing elements with indexing // returns deleted/empty array
+
+// var arr = [];
+// console.log(arr);
+// var input = prompt("enter item");
+// arr[0] = "1"; // direct inserting
+// arr.push("asdsad")
+// arr.push(1);
+// arr.push(false)
+// arr.push(["asdsad"])
+// arr.push(input);
+
+// console.log(arr);
+
+// arr.pop();
+// arr.pop();
+// arr.pop();
+// arr.pop();
+// arr.pop();
+
+// console.log(arr);
+
+// arr.push(1,2,23,23,232,323,23,"s")
+// arr.push("jjj")
+// console.log(arr);
+
+
+// arr.unshift("Ali")
+// arr.unshift("Faizan","Saad","Sami")
+
+
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// var arr = ['a','v','c','e'];
+
+// arr.splice(1,2,"Mj");
+
+// var arr = ['a','v','c','e'];
+// arr.splice(2,2,"ali")
+// var arr = ['a','v','c','e'];
+// arr.splice(0,3,"Sami");
+// arr.splice(2,0,"Ali")
+
+
+//for(condition){
+    //statement
+//}
+
+//1) declare+initialization //one time
+//2) condition
+//3) running state + unfamilar operators
+
+var name = "Hasnain Ali";
+for(var i=0;i<=100;i++ 
+    
+){console.log(i +"hasnan")}
