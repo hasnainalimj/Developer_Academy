959cf62c27b947f8fd5ba48aa1f1ff0ebb55c454
hasnainalimj <mjhasnain15@gmail.com>
2019-09-07 20:05:00 +0500

    Class 8

---
+++ JavaScript/Class 8/js/app.js
@@ -0,0 +1,76 @@
+// for(var i=5; i<73; i++){
+//     console.log("i");
+// }
+
+// var input = +prompt("Enter your number");
+
+// var found = false;
+
+// for (var i = 0; i < input; i++) {
+  // if(i == 10){
+  //     found = true;
+  // }
+// }
+
+// if(found == true){
+//     console.log("found");
+// }else{
+//     console.log("not found");
+// }
+
+// var arr = ["Hasnain" , "Ali" ,"Saad" , "Faizan" , "Sami" , ["A" , "B" , "C"]];
+
+// for(var i=0; i<arr.length; i++){
+// console.log(arr[i]);
+// }
+
+// var name = "Ali";
+// console.log(name[1]);
+
+// var n = 528;
+// console.log(n[0]);
+
+// var arr = [
+//     ["A" , "B" , "C"],
+//     ["D","E","F"]
+// ];
+// console.log(arr[0][1]);
+// for(var i=0; i<arr.length; i++){
+//     // console.log(arr[i]);
+//     for(var j=0; j<arr[i].length; j++){ 
+//         console.log(arr[i][j]);// i =0 , j = 0 , j = 1 , j = 2
+//     }
+// }
+
+var data = ["A" , "B" , "C" , "A" , "A"];
+var foundA = false;
+
+for(var i=0; i<data.length; i++){
+    if(data[i] == "A"){
+        foundA = true;
+    }
+}
+
+// if(foundA == true){
+//         console.log("api request");
+// }else{
+//         console.log("not found");
+// }
+
+var name = "Shan";
+
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+
+var num = 1;
+var arrays = [];
+var bool = false;
+
+console.log(typeof(name));
+console.log(typeof(num));
+console.log(typeof(arrays));
+console.log(typeof(bool));
+
+
+
+
