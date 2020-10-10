d0c454bb405121c616fd97b5749d6325ce4c7255
hasnainalimj <mjhasnain15@gmail.com>
2020-10-10 18:26:30 +0500

    Class 22 ES6

--- React/class_one 33/src/App.js
+++ React/class_one 33/src/App.js
@@ -1,18 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
-const text = "This is your first class";
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1 className="text">React Class 1</h1>
-        <h1>{text}</h1>
-      </div>
-    );
-  }
+const App = (props) => {
+  console.log("App -> props", props)
+  return (
+    <h1>ajklsdjklsajdkjsakldjklsajkl</h1>
+  )
 }
 
-export default App;
+export default App
